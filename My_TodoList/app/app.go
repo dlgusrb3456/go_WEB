24483d0ff168673b51004a3ee3e4261b7401f75d
dlgusrb3456 <dlgusrb3456@naver.com>
2023-03-22 18:10:07 +0900

    Refactoring My-Todolist detach storage & write test code to check refactoring

--- My_TodoList/app/app.go
+++ My_TodoList/app/app.go
@@ -5,7 +5,6 @@ import (
 	"go_WEB/My_TodoList/model"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
@@ -19,19 +18,21 @@ func redirectToMain(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTodoList(w http.ResponseWriter, r *http.Request) {
-	list := []*model.Todo{}
-	for _, v := range model.TodoMap {
-		list = append(list, v)
-	}
+	// list := []*model.Todo{}
+	// for _, v := range model.TodoMap {
+	// 	list = append(list, v)
+	// }
+	list := model.GetTodos()
 	rd.JSON(w, http.StatusOK, list)
 }
 
 func postTodoList(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name") //input_box로부터 받아온 내용
-	tempTodo := &model.Todo{ID: count, Name: name, Completed: false, CreatedAt: time.Now()}
+	tempTodo := model.AddTodo(name)
+	// tempTodo := &model.Todo{ID: count, Name: name, Completed: false, CreatedAt: time.Now()}
 
-	model.TodoMap[count] = tempTodo
-	count += 1
+	// model.TodoMap[count] = tempTodo
+	// count += 1
 	rd.JSON(w, http.StatusCreated, tempTodo)
 }
 
@@ -42,15 +43,22 @@ type Success struct {
 func deleteTodoList(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
+	ok := model.DeleteTodo(id)
 
-	_, ok := model.TodoMap[id]
-	if !ok {
-		rd.JSON(w, http.StatusOK, Success{false})
-	} else {
-		delete(model.TodoMap, id)
+	if ok {
 		rd.JSON(w, http.StatusOK, Success{true})
+	} else {
+		rd.JSON(w, http.StatusOK, Success{false})
 
 	}
+	// _, ok := model.TodoMap[id]
+	//  if !ok {
+	// 	rd.JSON(w, http.StatusOK, Success{false})
+	// } else {
+	// 	delete(model.TodoMap, id)
+	// 	rd.JSON(w, http.StatusOK, Success{true})
+
+	// }
 
 }
 
@@ -66,19 +74,28 @@ func completeTodoList(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	_, ok := model.TodoMap[id]
-	if ok {
-		if model.TodoMap[id].Completed {
-			model.TodoMap[id].Completed = false
-			rd.JSON(w, http.StatusOK, Complete{true, false})
-		} else {
-			model.TodoMap[id].Completed = true
-			rd.JSON(w, http.StatusOK, Complete{true, true})
-		}
-
-	} else {
+	ok_value := model.CompleteTodo(id)
+	if ok_value == 1 { //true to false
+		rd.JSON(w, http.StatusOK, Complete{true, false})
+	} else if ok_value == 2 { //false to true
+		rd.JSON(w, http.StatusOK, Complete{true, true})
+	} else { // no id in map
 		rd.JSON(w, http.StatusOK, Complete{false, false})
 	}
+
+	// _, ok := model.TodoMap[id]
+	// if ok {
+	// 	if model.TodoMap[id].Completed {
+	// 		model.TodoMap[id].Completed = false
+	// 		rd.JSON(w, http.StatusOK, Complete{true, false})
+	// 	} else {
+	// 		model.TodoMap[id].Completed = true
+	// 		rd.JSON(w, http.StatusOK, Complete{true, true})
+	// 	}
+
+	// } else {
+	// 	rd.JSON(w, http.StatusOK, Complete{false, false})
+	// }
 }
 
 func getInfoList(w http.ResponseWriter, r *http.Request) {
@@ -88,15 +105,15 @@ func getInfoList(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	v, ok := model.TodoMap[id]
-	if ok {
+	v, ok := model.GetInfo(id)
+	if ok { //존재하면
 		fmt.Println(&v)
 		rd.JSON(w, http.StatusOK, &v)
 	}
 }
 
 func NewRouter() http.Handler {
-	model.TodoMap = make(map[int]*model.Todo)
+	// model.TodoMap = make(map[int]*model.Todo)
 	rd = render.New()
 
 	mux := mux.NewRouter()
--- My_TodoList/model/model.go
+++ My_TodoList/model/model.go
@@ -11,6 +11,60 @@ type Todo struct {
 
 var TodoMap map[int]*Todo
 
+func init() {
+	TodoMap = make(map[int]*Todo)
+}
+
 func GetTodos() []*Todo {
-	return nil
+	list := []*Todo{}
+	for _, v := range TodoMap {
+		list = append(list, v)
+	}
+	return list
+}
+
+var count int = 0
+
+func AddTodo(name string) *Todo {
+	tempTodo := &Todo{Name: name, ID: count, Completed: false}
+	TodoMap[count] = tempTodo
+	count += 1
+	return tempTodo
+}
+
+func DeleteTodo(id int) bool {
+	_, ok := TodoMap[id]
+	if ok {
+		delete(TodoMap, id)
+		return true
+	} else {
+		return false
+	}
+}
+
+func CompleteTodo(id int) int { // 1 : true to false 2: false to true 3: non-exist
+	v, ok := TodoMap[id]
+	if ok {
+		complition := v.Completed
+		if complition == true {
+			v.Completed = false
+			return 1
+		} else {
+			v.Completed = true
+			return 2
+		}
+	} else {
+		return 3
+	}
+
+	return 1
+}
+
+func GetInfo(id int) (*Todo, bool) {
+	v, ok := TodoMap[id]
+	if ok {
+		return v, true
+	} else {
+		return nil, false
+	}
 }
